16e147b295a2f30dc41fadc7e82bb6cdb3e279c8
Debajyoti Bhakta <debajyotitech04@gmail.com>
2024-10-24 18:38:42 +0530

    Add files via upload

---
+++ Variable.js
@@ -0,0 +1,22 @@
+// Print Syntax
+console.log("Hey I'm learning Java Script")
+
+// Variables in JS
+
+var a=5
+var b=4
+var c="Yash"
+b=b+1
+console.log(b)
+console.log(a+b)
+console.log(a+b+9)
+console.log(typeof(a), typeof(c))
+
+// Constant in JS
+const d=9
+console.log(d)
+
+ // let a: number
+let e=54;
+console.log(e)
+
---
+++ condition.js
@@ -0,0 +1,21 @@
+console.log("Hello! I am Yash...")
+
+let age=17;
+let grace= 2;
+
+// Arithimatic Operation
+console.log(age+grace)
+console.log(age-grace)
+console.log(age/grace)
+console.log(age*grace)
+console.log(age**grace) //power 2
+
+// Conditional operation
+if((age+grace)>=18){
+  console.log("U r adult")
+}
+else{
+    console.log("Dudu pio bacche")
+}
+
+

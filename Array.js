8bb5a6a6fdb13be05b8fcb41ebdb8c3204c09db8
Debajyoti Bhakta <debajyotitech04@gmail.com>
2024-10-25 19:50:39 +0530

    Add files via upload

---
+++ Array.js
@@ -0,0 +1,14 @@
+// Array is muteable, so we can change Array
+// Array declaration => let arr: number[]
+let arr= [1,2,3,4,5]
+
+console.log(arr);
+console.log(arr.length);
+console.log(arr[2]);
+
+arr[1]=9;  //Mutability of array
+console.log(arr)
+
+let i=[498, 3498, 430, 60, 3]
+console.log(i.pop(60))
+console.log(i.push(0))
